40b18e6583de36945342b9402aebb9a61ca6c75f
Colin <colind2824@tuckahoeschools.org>
2024-11-07 13:30:05 +0000

    intial commit

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const yellowLight = document.querySelector('.yellow-light');
 const greenLight = document.querySelector('.green-light');
 const onLight = document.querySelector('.on-light')
 const offLight = document.querySelector('.off-light')
+
 //Use getElementById which allows us to get the ID so that we can add an event listener to the buttons, listen for a click and then set the color via css properties.
 const redSwitch = document.getElementById('red-switch');
 const yellowSwitch = document.getElementById('yellow-switch');
 const greenSwitch = document.getElementById('green-switch');
 const onSwitch = document.getElementById('on-switch')
-const offSwitch = document.getElementById('off-light')
+const offSwitch = document.getElementById('off-switch')
+const cycleSwitch = document.getElementById('cycle-switch')
 //Add event listeners that listen for a button click
 redSwitch.addEventListener("click",() => {
     redLight.style.backgroundColor = 'red';
@@ -23,13 +25,45 @@ greenSwitch.addEventListener("click", () => {
     greenLight.style.backgroundColor = 'green';
 
 });
-offSwitch.addEventListener("click ",() => {
 
-} ;)
 
 
 //Turn all lights on and off with their specific buttons
-
+offSwitch.addEventListener("click", () => {
+    redLight.style.backgroundColor = 'grey';
+    yellowLight.style.backgroundColor = 'grey';
+    greenLight.style.backgroundColor = 'grey';
+});
+onSwitch.addEventListener("click", () => {
+    redLight.style.backgroundColor = 'red';
+    yellowLight.style.backgroundColor = 'yellow';
+    greenLight.style.backgroundColor = 'green';
+});
 //Party Mode Button
 
+let currentLight = 'red';
+let intervalId = null;
+
+function cycleLight(){
+    redLight.style.backgroundColor = '#444';
+    yellowLight.style.backgroundColor = '#444';
+    greenLight.style.backgroundColor = '#444';
+    if(currentLight === 'red'){
+        redLight.style.backgroundColor = 'red';
+        currentLight = 'yellow';
+    }else if(currentLight ==='yellow'){
+        yellowLight.style.backgroundColor = 'yellow';
+        currentLight = 'green';
+    }else{
+        greenLight.style.backgroundColor = 'green';
+        currentLight = 'red';
+    }
+}
+
+cycleSwitch.addEventListener("click", () => {
+   if(!intervalId){
+    intervalId = setInterval(cycleLight,1000);
+   };
+   
+});
 //Turn all lights on and off at once with a single button
